99f41dbb4568dcb2d51c8348692ba99899351eb0
Thanus Kumaar A <thanuskumaara@gmail.com>
2025-09-20 00:08:03 +0530

    Basic setup of project

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+/*
+Then main file of the binary executable that will be compiled
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Starting the microservice...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Thanus-Kumaar/controller_microservice_v2
+
+go 1.24.1
