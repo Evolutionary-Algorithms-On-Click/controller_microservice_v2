250e055dac35960f2499769431c53548535a2fe8
Thanus Kumaar A <thanuskumaara@gmail.com>
2025-09-20 15:58:01 +0530

    Started with logger package

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,19 @@
 /*
 Then main file of the binary executable that will be compiled
 */
-
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/Thanus-Kumaar/controller_microservice_v2/pkg"
+)
 
 func main() {
-	fmt.Println("Starting the microservice...")
+	logger, err := pkg.NewLogger("DEVELOPMENT")
+	if err != nil {
+		log.Printf("[CRASH]: Logger initialization failed: %v", err)
+		return
+	}
+	logger.Info().Msg("[MSG]: Application starting...")
 }
---
+++ pkg/logger.go
@@ -0,0 +1,37 @@
+package pkg
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+	"github.com/rs/zerolog"
+)
+
+var Logger *zerolog.Logger
+
+func NewLogger(env string) (zerolog.Logger, error) {
+	var output io.Writer
+
+	switch env {
+	case "DEVELOPMENT":
+		// Use a human-readable console writer for development
+		output = zerolog.ConsoleWriter{
+			Out:        os.Stderr,
+			TimeFormat: time.RFC3339,
+		}
+	case "PRODUCTION":
+		// Use JSON output for production, which is machine-readable
+		// and can be easily consumed by log aggregation systems.
+		file, err := os.OpenFile("prod.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+		if err != nil {
+			return zerolog.Logger{}, err
+		}
+		output = file
+	default:
+		return zerolog.Logger{}, fmt.Errorf("invalid environment for logger setup: %s", env)
+	}
+
+	logger := zerolog.New(output).With().Timestamp().Logger()
+	return logger, nil
+}
