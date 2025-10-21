43d56c53c227042af29c1ca73cdd90fa02f8a026
Thanus Kumaar A <thanuskumaara@gmail.com>
2025-10-21 16:21:51 +0530

    Base for problem manager is setup

--- cmd/main.go
+++ cmd/main.go
@@ -5,46 +5,56 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/Thanus-Kumaar/controller_microservice_v2/db"
 	"github.com/Thanus-Kumaar/controller_microservice_v2/pkg"
 	jupyterclient "github.com/Thanus-Kumaar/controller_microservice_v2/pkg/jupyter_client"
+	"github.com/Thanus-Kumaar/controller_microservice_v2/routes"
+	"github.com/rs/cors"
 )
 
 func main() {
+	// Initialization of Logger
 	logger, err := pkg.NewLogger("DEVELOPMENT")
 	if err != nil {
 		log.Printf("[CRASH]: Logger initialization failed: %v", err)
 		return
 	}
 	pkg.Logger = &logger
-	pkg.Logger.Info().Msg("[MSG]: Application starting...")
+	pkg.Logger.Info().Msg("[MSG]: Logger initialization successful!")
+
+	// Initialization of Database
+	ctx := context.Background()
+	if err := db.InitDB(ctx); err != nil {
+		pkg.Logger.Fatal().Err(err).Msg("[CRASH]: Failed to initialize database connection")
+	}
+	defer db.Pool.Close()
+
+	// Initialization of HTTP Server
+	mux := http.NewServeMux()
+	routes.RegisterAPIRoutes(mux)
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // e.g. http://localhost:3000 or should get the frontend url from consul
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	}).Handler(mux)
+	addr := ":8080"
+	pkg.Logger.Info().Msg(fmt.Sprintf("Server listening on %s", addr))
+	if err := http.ListenAndServe(addr, corsHandler); err != nil {
+		pkg.Logger.Fatal().Err(err).Msg("[CRASH]: Failed to initialize the http server")
+	}
+
 	// since under development, the url and token is hardcoded
-	client, err := jupyterclient.NewClient("http://localhost:8888", "HelloThereHowAreyou!")
+	_, err = jupyterclient.NewClient("http://localhost:8888", "HelloThereHowAreyou!")
 	if err != nil {
 		pkg.Logger.Error().Msg(err.Error())
 		// should i return here? or just run the server until service is discovered?
 	}
 
-	// starting a kernel to see if it works
-	kernel2, err := client.StartKernel(context.Background(), "python3")
-	if err != nil {
-		pkg.Logger.Error().Msg(err.Error())
-	}
-	kernel1, err := client.StartKernel(context.Background(), "python3")
-	if err != nil {
-		pkg.Logger.Error().Msg(err.Error())
-	}
-	_, err = client.GetKernels(context.Background())
-	if err != nil {
-		pkg.Logger.Error().Msg(err.Error())
-	}
-	_, _ = client.GetKernelInfo(context.Background(), kernel1.ID)
-	_, _ = client.RestartKernel(context.Background(), kernel1.ID)
-	_ = client.DeleteKernel(context.Background(), kernel2.ID)
-	_ = client.DeleteKernel(context.Background(), kernel1.ID)
-	_, err = client.GetKernels(context.Background())
-	if err != nil {
-		pkg.Logger.Error().Msg(err.Error())
-	}
+	
 }
